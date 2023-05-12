8b378af8f4269b6e08b038bf1c45c6de946f11a6
MrcAdrn <4quinomarc@gmail.com>
2023-05-12 15:35:35 +0800

    remove color Orange on colors.xml DONE but still has no funtions

--- app/src/main/java/org/rmj/guanzongroup/gsecurity/registerActivity.java
+++ app/src/main/java/org/rmj/guanzongroup/gsecurity/registerActivity.java
@@ -2,16 +2,13 @@ package org.rmj.guanzongroup.gsecurity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.TextView;
 
-public class Activity_Login extends AppCompatActivity {
+public class registerActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        setContentView(R.layout.activity_register);
     }
 }
