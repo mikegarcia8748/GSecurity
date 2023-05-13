10c787a46b58625a4f604d2bf7c58b785633a37d
mangpeyts <jordanraymundo33@gmail.com>
2023-05-13 11:24:54 +0800

    Activity_LoadScreennn

--- app/src/main/java/org/rmj/guanzongroup/gsecurity/Activity_LoadScreen.java
+++ app/src/main/java/org/rmj/guanzongroup/gsecurity/Activity_LoadScreen.java
@@ -2,13 +2,27 @@ package org.rmj.guanzongroup.gsecurity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+
+import org.rmj.guanzongroup.gsecurity.Activities.MainActivity;
 
 public class Activity_LoadScreen extends AppCompatActivity {
-
+Handler handler;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_load_screen);
+        handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(Activity_LoadScreen.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        },5000);
+
     }
 }
