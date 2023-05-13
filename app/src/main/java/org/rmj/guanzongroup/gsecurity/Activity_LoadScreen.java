b4fa9d787da8c794674d5b33f071e9def257bdbb
mangpeyts <jordanraymundo33@gmail.com>
2023-05-13 09:36:00 +0800

    Added new activities for load screen

---
+++ app/src/main/java/org/rmj/guanzongroup/gsecurity/Activity_LoadScreen.java
@@ -0,0 +1,14 @@
+package org.rmj.guanzongroup.gsecurity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Activity_LoadScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_load_screen);
+    }
+}
