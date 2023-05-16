9ac02971454c8489ff5d1dac96745bbfb7554ab9
MrcAdrn <133180813+MrcAdrn@users.noreply.github.com>
2023-05-16 10:10:45 +0800

    dashboard1 -marc

--- app/src/main/java/org/rmj/guanzongroup/gsecurity/ActivityDashboard.java
+++ app/src/main/java/org/rmj/guanzongroup/gsecurity/ActivityDashboard.java
@@ -3,6 +3,10 @@ package org.rmj.guanzongroup.gsecurity;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
+
+import java.text.DateFormat;
+import java.util.Calendar;
 
 public class ActivityDashboard extends AppCompatActivity {
 
@@ -10,5 +14,11 @@ public class ActivityDashboard extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
+
+        Calendar calendar = Calendar.getInstance();
+        String currentdate = DateFormat.getDateInstance(DateFormat.FULL).format(calendar.getTime());
+
+        TextView date = findViewById(R.id.date);
+        date.setText(currentdate);
     }
 }
---
+++ app/src/test/java/dashboarditem.java
@@ -0,0 +1,37 @@
+public class dashboarditem {
+    String name;
+
+    public dashboarditem(String name, String email, int image) {
+        this.name = name;
+        this.email = email;
+        this.image = image;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getImage() {
+        return image;
+    }
+
+    public void setImage(int image) {
+        this.image = image;
+    }
+
+    String email;
+    int image;
+
+}
