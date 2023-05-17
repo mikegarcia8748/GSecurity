b9f883fe07a6098e17d67fa32de6ee24adb408b4
mangpeyts <jordanraymundo33@gmail.com>
2023-05-17 16:30:13 +0800

    login

--- app/src/main/java/org/rmj/guanzongroup/gsecurity/Activity_Login.java
+++ app/src/main/java/org/rmj/guanzongroup/gsecurity/Activity_Login.java
@@ -4,17 +4,15 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Patterns;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
-import android.widget.Toast;
 
 public class  Activity_Login extends AppCompatActivity {
 
-    TextView CreatNewAccount;
-    TextView forgotmypassword;
+    TextView createAnAccount;
+    TextView forgotpassword;
     EditText inputEmail, inputPassword;
     Button login;
 
@@ -23,45 +21,57 @@ public class  Activity_Login extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        forgotpassword = findViewById(R.id.forgotpassword);
         inputEmail = findViewById(R.id.inputEmail);
         inputPassword = findViewById(R.id.inputPassword);
         login = findViewById(R.id.login);
-
-
-        forgotmypassword=findViewById(R.id.forgotpassword);
-        forgotmypassword.setOnClickListener(new View.OnClickListener() {
+        createAnAccount = findViewById(R.id.createAnAccount);
+        forgotpassword.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivity(new Intent(Activity_Login.this,Activity_ForgotPassword.class));
+                startActivity(new Intent(Activity_Login.this, Activity_ForgotPassword.class));
             }
         });
 
-        CreatNewAccount=findViewById(R.id.createAnAccount);
-        CreatNewAccount.setOnClickListener(new View.OnClickListener() {
+        createAnAccount = findViewById(R.id.createAnAccount);
+        createAnAccount.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-             startActivity(new Intent(Activity_Login.this,Activity_CreateAccount.class ));
+                startActivity(new Intent(Activity_Login.this, Activity_CreateAccount.class));
             }
         });
         login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                validateEmailAddress(inputEmail);
+                String name1 = inputEmail.getText().toString();
+                String name2 = inputPassword.getText().toString();
 
+                validateinfo(name1, name2);
             }
         });
 
     }
-    private boolean validateEmailAddress(EditText email){
-        String emailInput = this.inputEmail.getText().toString();
 
-        if (!emailInput.isEmpty() && Patterns.EMAIL_ADDRESS.matcher(emailInput).matches()){
-            Toast.makeText(this, "Valid Email Successfully", Toast.LENGTH_SHORT).show();
-            return true;
-        }else {
-            Toast.makeText(this, "Invalid Email Address", Toast.LENGTH_SHORT).show();
+    private boolean validateinfo(String name1, String name2) {
+
+        if (name1.length() == 0) {
+            inputEmail.requestFocus();
+            inputEmail.setError("Field Cannot be empty");
             return false;
         }
-
+        else if (!name1.matches("[a-zA-Z0-9._-]+@[a-z]+\\.+[a-z]+")) {
+            inputEmail.requestFocus();
+            inputEmail.setError("Enter Valid Email!");
+            return false;
+        }
+        else if (name2.length() < 8 || name2.length() > 16) {
+            inputPassword.requestFocus();
+            inputPassword.setError("Password must be between 8-16 characters");
+            return false;
+        }
+        else {
+            return true;
+        }
     }
 }
