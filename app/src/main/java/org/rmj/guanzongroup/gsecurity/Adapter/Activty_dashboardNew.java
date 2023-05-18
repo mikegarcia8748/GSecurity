9d4c9cba3f122b94bbb4dbf8bfd5bc9a81923a6f
MrcAdrn <133180813+MrcAdrn@users.noreply.github.com>
2023-05-18 11:01:13 +0800

    fixing recycler viewer  -marc

---
+++ app/src/main/java/org/rmj/guanzongroup/gsecurity/Adapter/Activty_dashboardNew.java
@@ -0,0 +1,16 @@
+package org.rmj.guanzongroup.gsecurity.Adapter;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import org.rmj.guanzongroup.gsecurity.R;
+
+public class Activty_dashboardNew extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_activty_dashboard_new);
+    }
+}
