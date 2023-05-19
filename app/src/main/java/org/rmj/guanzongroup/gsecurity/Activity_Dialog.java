0cfc3724bd3f7fc68957c2dc4af648cc73951a4e
mangpeyts <jordanraymundo33@gmail.com>
2023-05-19 11:35:45 +0800

    dialog and icon

---
+++ app/src/main/java/org/rmj/guanzongroup/gsecurity/Activity_Dialog.java
@@ -0,0 +1,62 @@
+package org.rmj.guanzongroup.gsecurity;
+
+import android.annotation.SuppressLint;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+
+
+public class Activity_Dialog extends AppCompatActivity {
+
+    ImageButton cancelButton;
+    Button okbtn;
+    @SuppressLint("MissingInflatedId")
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dialog);
+
+
+        View alertCustomDialog = LayoutInflater.from(Activity_Dialog.this).inflate(R.layout.custom_dialog,null);
+        AlertDialog.Builder alertDialog = new AlertDialog.Builder(Activity_Dialog.this);
+
+        alertDialog.setView(alertCustomDialog);
+        cancelButton = (ImageButton) alertCustomDialog.findViewById(R.id.cancelId);
+        okbtn = (Button) alertCustomDialog.findViewById(R.id.ok_btn);
+
+        final AlertDialog dialog = alertDialog.create();
+
+        findViewById(R.id.show_id).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+                dialog.show();
+            }
+        });
+
+
+
+
+        cancelButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dialog.cancel();
+            }
+        });
+        okbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dialog.cancel();
+                Toast.makeText(Activity_Dialog.this, "...", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+}
