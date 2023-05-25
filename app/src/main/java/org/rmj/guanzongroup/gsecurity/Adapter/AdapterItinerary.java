defe5aeba13c6d8d3d76f5b07964b26c7e1c09dc
MrcAdrn <133180813+MrcAdrn@users.noreply.github.com>
2023-05-25 16:32:35 +0800

    done qr code alert box /

--- app/src/main/java/org/rmj/guanzongroup/gsecurity/Adapter/AdapterItinerary.java
+++ app/src/main/java/org/rmj/guanzongroup/gsecurity/Adapter/AdapterItinerary.java
@@ -1,16 +1,17 @@
 package org.rmj.guanzongroup.gsecurity.Adapter;
 
-import android.content.Intent;
+import android.app.AlertDialog;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.google.android.material.textview.MaterialTextView;
 
-import org.rmj.guanzongroup.gsecurity.Dialog.DialogMessage;
 import org.rmj.guanzongroup.gsecurity.R;
 
 import java.util.List;
@@ -35,15 +36,10 @@ public class AdapterItinerary extends RecyclerView.Adapter<AdapterItinerary.VHPe
         String lsPersonnel = poPersonnels.get(position);
         holder.lblPersonx.setText(lsPersonnel);
 
-        // Set OnClickListener for the item view
         holder.itemView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                // Handle the click event here
-                // For example, you can show a dialog or start a new activity
-                // You can replace the code below with your desired implementation
-                Intent intent = new Intent(v.getContext(), DialogMessage.class);
-                v.getContext().startActivity(intent);
+                showScanDialog(v);
             }
         });
     }
@@ -61,5 +57,26 @@ public class AdapterItinerary extends RecyclerView.Adapter<AdapterItinerary.VHPe
             super(itemView);
             lblPersonx = itemView.findViewById(R.id.lbl_personnel);
         }
+    }
+
+    private void showScanDialog(View view) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(view.getContext());
+        View dialogView = LayoutInflater.from(view.getContext()).inflate(R.layout.messagedialog, null);
+
+        builder.setView(dialogView);
+
+        AlertDialog dialog = builder.create();
+        dialog.show();
+
+        ImageButton btnCancel = dialogView.findViewById(R.id.cancelId);
+        Button btnPositive = dialogView.findViewById(R.id.btnPositive);
+        Button btnNegative = dialogView.findViewById(R.id.btnNegative);
+
+        btnCancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dialog.dismiss();
+            }
+        });
     }
 }
