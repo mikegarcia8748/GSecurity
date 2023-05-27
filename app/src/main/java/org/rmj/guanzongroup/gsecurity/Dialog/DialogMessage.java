4998b9fe67b460718535a2743fc8fcea156e360a
MrcAdrn <133180813+MrcAdrn@users.noreply.github.com>
2023-05-27 08:47:14 +0800

    all done ready for the next....... - marc

--- app/src/main/java/org/rmj/guanzongroup/gsecurity/Dialog/DialogMessage.java
+++ app/src/main/java/org/rmj/guanzongroup/gsecurity/Dialog/DialogMessage.java
@@ -5,7 +5,6 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
-import android.widget.ImageButton;
 import android.widget.TextView;
 
 import androidx.annotation.Nullable;
@@ -48,7 +47,7 @@ public class DialogMessage extends AppCompatActivity {
         View dialogView = LayoutInflater.from(this).inflate(R.layout.scanned, null);
 
 
-        ImageButton cancelBtn = dialogView.findViewById(R.id.cancel);
+
         TextView titleTextView = dialogView.findViewById(R.id.title);
         TextView messageTextView = dialogView.findViewById(R.id.message);
 
